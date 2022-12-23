286e90c98b7a95891be991e8bfd63a178a94557d
bvivess <bvives@iesemilidarder.com>
2022-12-23 10:11:26 +0100

    Inicial

---
+++ index.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="estils.css">
+	</head>
+	<body>
+
+		<div id="header">
+		  <h1>Human Resource</h1>
+		</div>
+
+			<div id="content">
+
+			  <div id="menu">
+				<ul>
+				  <li>Employees</li>
+				  <li>Departments</li>
+				  <li>Jobs</li>
+				  <li>Others</li>
+				</ul>
+			  </div>
+
+			  <div id="section">
+				<h1>Get ready !!</h1>
+
+			  </div>
+		</div>
+
+		<div id="footer">
+		  <p>(c) BvS - 2022</p>
+		</div>
+
+	</body>
+</html>
